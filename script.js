d8a265b95e9fbac5e4cbcf13c5fe72084873e703
mubarakaataye <atayemubaraka@gmail.com>
2024-12-31 20:35:44 +0430

    1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('creat-account-btn').addEventListener('click' ,function()
+ {document.getElementById('login-form').style.display = 'none';
+    document.getElementById('register-form').style.display = 'block';
+ });
